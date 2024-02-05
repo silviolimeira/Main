851c6eca38b82da2f661c35de968631e16664897
Silvio Limeira <sicemal@gmail.com>
2024-02-04 21:24:40 -0300

    Correcoes

--- Identidade/init-basegeografica.sh
+++
@@ -1,2 +0,0 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SqlServer2022! -d master -i /tmp/01-BaseGeografica.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SqlServer2022! -d master -i /tmp/02-BaseGeografica.sql
--- Identidade/init-database.sh
+++
@@ -1,2 +0,0 @@
-sleep 60s
-/opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P SqlServer2022! -d master -i /tmp/BKPIdentidadeDB.sql
