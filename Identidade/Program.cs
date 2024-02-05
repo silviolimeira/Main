b365e2fe0e5c3cc4d38a09983a80fe1c5a3bbb34
Silvio Limeira <sicemal@gmail.com>
2024-02-05 18:25:48 -0300

    Add service swagger open api

--- Identidade/Program.cs
+++ Identidade/Program.cs
@@ -14,6 +14,7 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UsuarioDbContext>(options =>
@@ -70,7 +71,19 @@ builder.Services.AddAuthorization(options =>
 });
 
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 // Configure the HTTP request pipeline.
 
